dfb926369d7d371494e5ce7d7cb69b2ab10d5c95
ManhDuc85 <phamdomanhduc@gmail.com>
2026-01-07 21:18:55 +0700

    Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import path from "path";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
